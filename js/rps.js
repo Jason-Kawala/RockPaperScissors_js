8d3d0c9f4dd4ee50118a9cfd51c4c03a47a499aa
Shunji54 <kawalajason@gmail.com>
2022-01-18 16:22:42 +0100

    comments

--- js/rps.js
+++ js/rps.js
@@ -1,23 +1,24 @@
 inputNumber = Number(prompt("How much game do you want to play ? :"));
 playGame(inputNumber);
 
-
-function computerPlay() {
+// Computer make a 'choice'
+function computerPlay() { 
     var elements = ['rock','paper','scissors'];
     let choice = Math.floor(Math.random()*elements.length);
     return elements[choice];
 }
 
+// Player choose between Rock Paper and Scissors
 function playerSelection() {
     var choice = prompt('Choose between Rock(r) Paper(p) and Scissors(s)').charAt(0).toLowerCase();
     var elements = ['rock','paper','scissors'];
 
     while (choice != 'r' && choice != 'p' && choice != 's') {
-    if (choice != 'r' && choice != 'p' && choice != 's') {
-        var choice = prompt('Please choose between Rock(r) Paper(p) and Scissors(s)').charAt(0).toLowerCase();
-    } else {
-        break;
-    }
+        if (choice != 'r' && choice != 'p' && choice != 's') {
+            var choice = prompt('Please choose between Rock(r) Paper(p) and Scissors(s)').charAt(0).toLowerCase();
+        } else {
+            break;
+        }
     }
 
     switch(choice) {
@@ -35,22 +36,23 @@ function playGame(x) {
     let computerScore = 0;
 
     for (let i = 0; i<x; i++) {
-        let game = playRound();
+        let game = playRound(); // Play a round and store the returned result to this variable
         switch (game) {
-            case 0:
+            case 0: // Tie
                 console.log('Tie');
                 break;
-            case 1:
+            case 1: // Computer wins
                 computerScore += 1;
                 console.log(`${playerScore} to ${computerScore}`);
                 break;
-            case 2:
+            case 2: // Player wins
                 playerScore += 1;
                 console.log(`${playerScore} to ${computerScore}`);
                 break;
         }
     }
 
+    // Check who won the game
     if (playerScore == computerScore) {
         console.log('Oh, that was a tough one, tie !');
     } else if (playerScore > computerScore) {
