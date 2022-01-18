de7eba600bfa2a76a31fbb114610a36ca1f30694
Shunji54 <kawalajason@gmail.com>
2022-01-18 16:12:19 +0100

    main function to loop game based on user input

--- js/rps.js
+++ js/rps.js
@@ -1,4 +1,6 @@
-console.log('Start');
+inputNumber = Number(prompt("How much game do you want to play ? :"));
+playGame(inputNumber);
+
 
 function computerPlay() {
     var elements = ['rock','paper','scissors'];
@@ -7,12 +9,12 @@ function computerPlay() {
 }
 
 function playerSelection() {
-    var choice = prompt('Choose between Rock(r) Paper(p) and Scissors(s)').charAt(0);
+    var choice = prompt('Choose between Rock(r) Paper(p) and Scissors(s)').charAt(0).toLowerCase();
     var elements = ['rock','paper','scissors'];
 
     while (choice != 'r' && choice != 'p' && choice != 's') {
     if (choice != 'r' && choice != 'p' && choice != 's') {
-        var choice = prompt('Please choose between Rock(r) Paper(p) and Scissors(s)').charAt(0);
+        var choice = prompt('Please choose between Rock(r) Paper(p) and Scissors(s)').charAt(0).toLowerCase();
     } else {
         break;
     }
@@ -28,40 +30,79 @@ function playerSelection() {
     }
 }
 
-function playGame(x=1) {
-    const computer = computerPlay()
-    const player = playerSelection()
+function playGame(x) {
+    let playerScore = 0;
+    let computerScore = 0;
 
-    console.log(`You choose ${player} and computer choose ${computer}\n`)
+    for (let i = 0; i<x; i++) {
+        let game = playRound();
+        switch (game) {
+            case 0:
+                console.log('Tie');
+                break;
+            case 1:
+                computerScore += 1;
+                console.log(`${playerScore} to ${computerScore}`);
+                break;
+            case 2:
+                playerScore += 1;
+                console.log(`${playerScore} to ${computerScore}`);
+                break;
+        }
+    }
+
+    if (playerScore == computerScore) {
+        console.log('Oh, that was a tough one, tie !');
+    } else if (playerScore > computerScore) {
+        console.log(`Congratulation, you won ! ${playerScore} to ${computerScore}`);
+    } else {
+        console.log(`You lost.. ${playerScore} to ${computerScore}`);
+    }
+}
+
+function playRound() {
+    const player = playerSelection();
+    const computer = computerPlay();
+
+    console.log(`You choose ${player} and computer choose ${computer}\n`);
 
     if (player == 'rock') {
         if (computer == 'rock') {
-            console.log('This is a tie !')
+            console.log('This is a tie !');
+            return 0;
         } else if (computer == 'paper') {
-            console.log('You lost !')
+            console.log(`You lost ! ${computer} beats ${player}`);
+            return 1;
         } else {
-            console.log('You won !')
+            console.log(`You won ! ${player} beats ${computer}`);
+            return 2;
         }
     }
 
     if (player == 'paper') {
         if (computer == 'paper') {
-            console.log('This is a tie !')
+            console.log('This is a tie !');
+            return 0;
         } else if (computer == 'scissors') {
-            console.log('You lost !')
+            console.log(`You lost ! ${computer} beats ${player}`);
+            return 1;
         } else {
-            console.log('You won !')
+            console.log(`You won ! ${player} beats ${computer}`);
+            return 2;
         }
     }
 
     if (player == 'scissors') {
         if (computer == 'scissors') {
-            console.log('This is a tie !')
+            console.log('This is a tie !');
+            return 0;
         } else if (computer == 'rock') {
-            console.log('You lost !')
+            console.log(`You lost ! ${computer} beats ${player}`);
+            return 1;
         } else {
-            console.log('You won !')
+            console.log(`You won ! ${player} beats ${computer}`);
+            return 2;
         }
     }
 
-} 
+}
