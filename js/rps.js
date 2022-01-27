a9ac7d6e4825845cbc55158059812e388e6d5a51
Shunji54 <kawalajason@gmail.com>
2022-01-27 23:44:53 +0100

    comment not used functions (old console game)

--- js/rps.js
+++ js/rps.js
@@ -17,6 +17,8 @@ icons.forEach(icon => {
 })
 
 
+
+
 // Computer make a 'choice'
 function computerPlay() {
     var elements = ['rock','paper','scissors'];
@@ -25,50 +27,50 @@ function computerPlay() {
 }
 
 // Player choose between Rock Paper and Scissors
-function playerSelection(choice) {
-    var elements = ['rock','paper','scissors'];
+// function playerSelection(choice) {
+//     var elements = ['rock','paper','scissors'];
 
-    switch(choice) {
-        case 'rock':
-            return elements[0];
-        case 'paper':
-            return elements[1];
-        case 'scissors':
-            return elements[2];
-    }
-}
+//     switch(choice) {
+//         case 'rock':
+//             return elements[0];
+//         case 'paper':
+//             return elements[1];
+//         case 'scissors':
+//             return elements[2];
+//     }
+// }
 
 // Play game where x is the number of rounds given
-function playGame(x) {
-    let playerScore = 0;
-    let computerScore = 0;
+// function playGame(x) {
+//     let playerScore = 0;
+//     let computerScore = 0;
 
-    for (let i = 0; i<x; i++) {
-        let game = playRound(); // Play a round and store the returned result to this variable
-        switch (game) {
-            case 0: // Tie
-                console.log('Tie');
-                break;
-            case 1: // Computer wins
-                computerScore += 1;
-                console.log(`${playerScore} to ${computerScore}`);
-                break;
-            case 2: // Player wins
-                playerScore += 1;
-                console.log(`${playerScore} to ${computerScore}`);
-                break;
-        }
-    }
+//     for (let i = 0; i<x; i++) {
+//         let game = playRound(); // Play a round and store the returned result to this variable
+//         switch (game) {
+//             case 0: // Tie
+//                 console.log('Tie');
+//                 break;
+//             case 1: // Computer wins
+//                 computerScore += 1;
+//                 console.log(`${playerScore} to ${computerScore}`);
+//                 break;
+//             case 2: // Player wins
+//                 playerScore += 1;
+//                 console.log(`${playerScore} to ${computerScore}`);
+//                 break;
+//         }
+//     }
 
-    // Check who won the game
-    if (playerScore == computerScore) {
-        console.log('Oh, that was a tough one, tie !');
-    } else if (playerScore > computerScore) {
-        console.log(`Congratulation, you won ! ${playerScore} to ${computerScore}`);
-    } else {
-        console.log(`You lost.. ${playerScore} to ${computerScore}`);
-    }
-}
+//     // Check who won the game
+//     if (playerScore == computerScore) {
+//         console.log('Oh, that was a tough one, tie !');
+//     } else if (playerScore > computerScore) {
+//         console.log(`Congratulation, you won ! ${playerScore} to ${computerScore}`);
+//     } else {
+//         console.log(`You lost.. ${playerScore} to ${computerScore}`);
+//     }
+// }
 
 // one round of the game and return 0 1 or 2 based on user result -- 0-tie 1-lost 2-won
 function playRound(player, computer) {
